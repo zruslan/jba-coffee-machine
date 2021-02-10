6ebf9c82584bfd3281dc572d3c126a6ae5dd4999
zrus <->
2021-02-10 18:27:24 +0700

    stage 2

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,9 +1,20 @@
 # Write your code here
-print(
-"""Starting to make a coffee
+coffee = (200, 50, 15)
+
+print("Write how many cups of coffee you will need:")
+cups = int(input())
+plural = "s" if cups != 1 else""
+
+print(f"For {cups} cup{plural} of coffee you will need:")
+
+print("{} ml of water".format(cups * coffee[0]))
+print("{} ml of milk".format(cups * coffee[1]))
+print("{} g of coffee beans".format(cups * coffee[2]))
+
+s = """Starting to make a coffee
 Grinding coffee beans
 Boiling water
 Mixing boiled water with crushed coffee beans
 Pouring coffee into the cup
 Pouring some milk into the cup
-Coffee is ready!""")
+Coffee is ready!"""
